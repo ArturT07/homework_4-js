c7bd4e9cf99f724cbd1c8722003931050245eff8
Toba Artur <toba.artur@gmail.com>
2023-11-07 18:22:07 +0200

    homework

---
+++ scripts.js
@@ -0,0 +1,58 @@
+fetch("https://ajax.test-danit.com/api/swapi/films")
+    .then(response => response.json())
+    .then(data => {
+      // Отримали список фільмів
+      displayFilms(data);
+    })
+    .catch(error => {
+      console.error("Помилка отримання списку фільмів: " + error);
+    });
+
+function displayFilms(films) {
+    const filmList = document.getElementById("film-list");
+
+    // Сортуємо фільми за номером епізоду
+    films.sort((a, b) => a.episodeId - b.episodeId);
+
+    films.forEach(film => {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `Епізод ${film.episodeId}: ${film.name}<br>${film.openingCrawl}`;
+        listItem.addEventListener("click", () => {
+            // При натисканні на фільм виконуємо запит на отримання персонажів
+            fetchCharacters(film);
+        });
+
+        filmList.appendChild(listItem);
+    });
+}
+
+
+function fetchCharacters(film) {
+  const characters = film.characters;
+  const characterPromises = characters.map(characterUrl => fetch(characterUrl));
+
+  Promise.all(characterPromises)
+      .then(responses => Promise.all(responses.map(response => response.json())))
+      .then(characterData => {
+        // Отримали дані про персонажів фільму
+        displayCharacters(film, characterData);
+      })
+      .catch(error => {
+        console.error("Помилка отримання персонажів: " + error);
+      });
+}
+
+function displayCharacters(film, characters) {
+  const characterList = document.getElementById("character-list");
+  characterList.innerHTML = "";
+
+  const filmTitle = document.createElement("h2");
+  filmTitle.innerHTML = `Персонажі фільму "${film.name}"`;
+  characterList.appendChild(filmTitle);
+
+  characters.forEach(character => {
+    const characterName = document.createElement("p");
+    characterName.innerHTML = character.name;
+    characterList.appendChild(characterName);
+  });
+}
